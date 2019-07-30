79e229cc1f4b522529e20cee4f7ffb52b517882c
pc-pc1 <pc-pc1@pc>
2019-07-30 09:32:53 -0500

    Version 1

--- ManejoINterfaceGUI/src/ec/gob/cuerpoagentes/vista/Aplicacion.java
+++ ManejoINterfaceGUI/src/ec/gob/cuerpoagentes/vista/Aplicacion.java
@@ -1,20 +1,29 @@
 package ec.gob.cuerpoagentes.vista;
 
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class Aplicacion {
 
     public static void main(String[] arg) {
-        String nombre = JOptionPane.showInputDialog("Ingresa tus datos");
-        
-        String[] sep = nombre.split("\\s");
-        for (int i = 0; i < sep.length; i++) {
-            System.out.println(sep[i]);
+        String nombre = " ";
+        int conf = 0;
+        ArrayList<String> listaNombres = new ArrayList<>();
+
+        do {
+            nombre = JOptionPane.showInputDialog("INgresa tud datos").toUpperCase();
+            listaNombres.add(nombre);
+            conf = (JOptionPane.showConfirmDialog(null, "Deseas continuar"));
+
+        } while (conf == 0);
+
+        for (int i = 0; i < listaNombres.size(); i++) {
+
+            // solo nombres con s    if (listaNombres.get(i).charAt(0) == 'S' || listaNombres.get(i).charAt(0) == 's') {
+                
+                JOptionPane.showMessageDialog(null, listaNombres.get(i));
+            }
         }
-        String n1="Nombre: "+sep[0];
-        String ap1="Apellido 1: "+sep[1];
-        JOptionPane.showMessageDialog(null,n1+"\n"+ ap1);
-        
     }
 
 }
